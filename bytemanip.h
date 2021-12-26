f04c0cb0bb8698e629d96674e562ffcfd8a52cff
blixon <xawier700@gmail.com>
2021-12-26 16:49:02 +0100

    finished file structure

--- bytemanip.h
+++ bytemanip.h
@@ -2,16 +2,23 @@
 
 #include <iostream>
 #include <fstream>
-int32_t getBytes(short byteNumber, std::ifstream& image);
 
-int32_t getBytes(short byteNumber, std::ifstream& image)
+int32_t getBytes(short byteNumber, std::ifstream& file);
+
+int32_t get2Bytes(std::ifstream& file);
+
+int32_t get4Bytes(std::ifstream& file);
+
+void skipBytes(short byteNumber, std::ifstream& file);
+
+int32_t getBytes(short byteNumber, std::ifstream& file)
 {
     int32_t value = 0;
     int32_t excludes = 0;
     int32_t bytes[byteNumber];
 
     for (int i = 0; i < byteNumber; i++) {
-        bytes[i] = image.get();
+        bytes[i] = file.get();
     }
 
     for (int i = byteNumber - 1; i >= 0; i--)
@@ -23,4 +30,14 @@ int32_t getBytes(short byteNumber, std::ifstream& image)
     for (int i = 0; i < byteNumber - excludes; i++)
         value |= bytes[i] << (byteNumber - excludes - 1 - i) * 8;
     return value;
+}
+
+int32_t get2Bytes(std::ifstream& file) { return getBytes(2, file); }
+
+int32_t get4Bytes(std::ifstream& file) { return getBytes(4, file); }
+
+void skipBytes(short byteNumber, std::ifstream& file)
+{
+    for (int i = 0; i < byteNumber; i++)
+        file.get();
 }
--- main.cpp
+++ main.cpp
@@ -10,14 +10,44 @@ using namespace std;
 int main()
 {
     ifstream bmp("joker.bmp");
+
+    //START OF BITMAP-FILE-HEADER
+    //CHECKING IF THE FILE IS ACTUALLY BITMAP FILE
+
     if(!bmp.is_open())
         return 404;
-
     if(bmp.get() != 'B' || bmp.get() != 'M')
         return 1;
 
-    u_int32_t fileByteSize = getBytes(4, bmp);
-    cout<<fileByteSize<<endl;
+    u_int32_t fileByteSize;
+    u_int32_t pixelArrayOffset;
+
+
+    fileByteSize = get4Bytes(bmp);
+    skipBytes(4, bmp);
+    pixelArrayOffset = get4Bytes(bmp);
+
+    //END OF BITMAP-FILE-HEADER
+
+    //START OF BITMAP-INFO-HEADER
+    u_int32_t pixelWidth;
+    u_int32_t pixelHeight;
+    u_int32_t colorDepth;
+    u_int32_t compression;
+    u_int32_t imageSize;
+
+    skipBytes(4, bmp);
+    pixelWidth = get4Bytes(bmp);
+    pixelHeight = get4Bytes(bmp);
+    skipBytes(2, bmp);
+    colorDepth = get2Bytes(bmp);
+    compression = get4Bytes(bmp);
+    imageSize = get4Bytes(bmp);
+
+    skipBytes(8, bmp); //0x26 (4) and 0x2A (4) (horizontal and vertical resolution of the image (pixel per metre))
+
+    skipBytes(8, bmp); // 0x2E (4) and 0x32 (4) (the number of color in the color paletter and the number of important colors used)
+    //END OF BITMAP-INFO-HEADER
 
 
     cout<<"END\n";
